f263e954ab8bb80197be40d78fbc1189347fef3b
kingsley <kingsleykay26@gmail.com>
2026-04-10 23:18:24 +0100

    feat(todo): implement task completion toggle and update UI state

--- script.js
+++ script.js
@@ -3,6 +3,9 @@ import utc from "https://esm.sh/dayjs/plugin/utc";
 dayjs.extend(utc);
 
 const dueTimeElem = document.querySelector(".due-date");
+const taskElem = document.querySelector(".todo-title");
+const inputElem = document.querySelector("#checkbox");
+const todoStatusElem = document.querySelector(".todo-status");
 
 const dueLocalTime = dayjs("2026-04-16 23:59", "YYYY-MM-DD HH:mm");
 const utcTime = dueLocalTime.utc().format();
@@ -13,6 +16,15 @@ const dueTimeFormatted = dayjs(utcTime).format("MMM DD,YYYY HH:mm");
 
 const dueTimeHTML = `Due ${dueTimeFormatted}`;
 
-console.log(dueTimeHTML);
-
 dueTimeElem.innerHTML = dueTimeHTML;
+
+function handleTaskCompletion() {
+  taskElem.classList.toggle("completed-title-style");
+  todoStatusElem.classList.toggle("completed-status");
+
+  todoStatusElem.textContent = inputElem.checked ? "Done" : "In Progress";
+}
+
+inputElem.addEventListener("change", () => {
+  handleTaskCompletion();
+});
