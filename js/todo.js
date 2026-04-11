33bc12f96a85bba700ccd9155e7a31cdef51c415
kingsley <kingsleykay26@gmail.com>
2026-04-11 12:09:15 +0100

    feat(todo): hide due label via CSS class on task completion

--- js/todo.js
+++ js/todo.js
@@ -3,15 +3,19 @@ import { startDueLabelUpdate, stopDueLabelUpdate } from "./time.js";
 const todoStatusElem = document.querySelector(".todo-status");
 const taskElem = document.querySelector(".todo-title");
 const inputElem = document.querySelector("#checkbox");
+const dueLabelElem = document.querySelector(".remaining-time");
 
 export function handleTaskCompletion() {
   taskElem.classList.toggle("completed-title-style");
   todoStatusElem.classList.toggle("completed-status");
 
   const isCompleted = inputElem.checked;
-  if (isCompleted) stopDueLabelUpdate();
-  else {
+  if (isCompleted) {
+    stopDueLabelUpdate();
+    dueLabelElem.classList.add("end-timer");
+  } else {
     startDueLabelUpdate();
+    dueLabelElem.classList.remove("end-timer");
   }
   todoStatusElem.textContent = inputElem.checked ? "Done" : "In Progress";
 }
