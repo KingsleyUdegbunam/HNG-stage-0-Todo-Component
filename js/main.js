c2f48a7fa46152437392881566b486e018f6b9d4
kingsley <kingsleykay26@gmail.com>
2026-04-14 18:44:37 +0100

    feat(todo): add function to update todo card UI

--- js/main.js
+++ js/main.js
@@ -9,6 +9,14 @@ inputElem.addEventListener("change", handleTaskCompletion);
 
 buttonContainerElem.addEventListener("click", handleButtonClick);
 
+function updateTodoCard(todo) {
+  document.querySelector(".todo-title").textContent = todo.title;
+  document.querySelector(".todo-desc").textContent = todo.description;
+  document.querySelector(".priority").textContent = todo.priority;
+  document.querySelector(".due-date").textContent = todo.dueDate;
+  document.querySelector(".due-date").setAttribute("datetime", todo.dueDate);
+}
+
 getDueLabel();
 startDueLabelUpdate();
 handleFormSubmission();
