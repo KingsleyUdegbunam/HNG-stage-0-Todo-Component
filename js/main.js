748ea792e11fecd196ed4806ba6da48292d21d61
kingsley <kingsleykay26@gmail.com>
2026-04-15 07:30:31 +0100

    feat(todo): convert du date time input to displayable format on save

--- js/main.js
+++ js/main.js
@@ -1,5 +1,5 @@
 import { handleButtonClick, handleTaskCompletion } from "./todo.js";
-import { getDueLabel, startDueLabelUpdate } from "./time.js";
+import { getDueLabel, startDueLabelUpdate, toHTMLDate } from "./time.js";
 import { handleFormSubmission } from "./edit.js";
 
 const inputElem = document.querySelector("#checkbox");
@@ -13,7 +13,7 @@ function updateTodoCard(todo) {
   document.querySelector(".todo-title").textContent = todo.title;
   document.querySelector(".todo-desc").textContent = todo.description;
   document.querySelector(".priority").textContent = todo.priority;
-  document.querySelector(".due-date").textContent = todo.dueDate;
+  document.querySelector(".due-date").textContent = toHTMLDate(todo.dueDate);
   document.querySelector(".due-date").setAttribute("datetime", todo.dueDate);
 }
 
